f7e1b6fd2a15594e3807840e8e4fa1c4dcbb842d
khpark0203 <khpark0203@gmail.com>
2025-08-17 19:09:06 +0900

    feat: kafka init

--- monitoring-api/node/src/main/java/com/monitoring/node/v1/cpu/consumer/CpuConsumer.java
+++ monitoring-api/node/src/main/java/com/monitoring/node/v1/cpu/consumer/CpuConsumer.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class CpuConsumer {
     @KafkaListener(
-        topics = "cpuinfok",
+        topics = "cpuinfo",
         groupId = "monitoring-cpu",
         containerFactory = "cpuMessageConcurrentKafkaListenerContainerFactory"
     )
--- monitoring-infra/kafka/src/main/java/com/monitoring/kafka/producer/KafkaMessageProducer.java
+++ monitoring-infra/kafka/src/main/java/com/monitoring/kafka/producer/KafkaMessageProducer.java
@@ -1,27 +1,20 @@
 package com.monitoring.kafka.producer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
 @Component
 public class KafkaMessageProducer {
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public KafkaMessageProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaMessageProducer(KafkaTemplate<String, Object> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void send(String topic, Objects message) {
-        try {
-            String jsonMessage = new com.fasterxml.jackson.databind.ObjectMapper().writeValueAsString(message);
-            kafkaTemplate.send(topic, jsonMessage);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        kafkaTemplate.send(topic, message);
     }
 }
 
